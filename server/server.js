a46a4080cca04328048a9d9fc85eb51e162eb925
ffreyli <jeffster123@gmail.com>
2026-01-06 14:49:24 -0800

    added cloudflare domain as cors origin

--- server/server.js
+++ server/server.js
@@ -11,7 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: [
         "http://localhost:3000",
-        "https://ffreyli.github.io"
+        "https://ffreyli.github.io",
+        "https://pokemon-ev-trainer.jeffrey-li.com"
     ],
     credentials: true
 }));
